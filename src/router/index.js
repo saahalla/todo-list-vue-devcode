7bc0865823b5e231a2e81ed2aedc1575cc138067
Sahal Mahfudh <sahalmahfudh07@gmail.com>
2021-10-27 00:44:10 +0700

    [Sahal] - Test - Update Lazy Load

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Dashboard from '../views/Todo/Dashboard.vue'
-import Activity from '../views/Todo/Activity.vue'
+// import Home from '../views/Home.vue'
+const Home = () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
+// import Dashboard from '../views/Todo/Dashboard.vue'
+const Dashboard = () => import(/* webpackChunkName: "Dashboard" */ '../views/Todo/Dashboard.vue')
+// import Activity from '../views/Todo/Activity.vue'
+const Activity = () => import(/* webpackChunkName: "Activity" */ '../views/Todo/Activity.vue')
 
 Vue.use(VueRouter)
 
